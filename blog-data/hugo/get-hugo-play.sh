3452007823e4ad3a28ff89f53af05b99a4ac3749
J . Norment <cognitiaclaeves@gmail.com>
2017-04-29 23:10:50 +0000

    Working out a transient dev environment.

---
+++ blog-data/hugo/get-hugo-play.sh
@@ -0,0 +1,3 @@
+
+git clone https://github.com/cognitiaclaeves/hugo-play.git
+
--- run-container.sh
+++ run-container.sh
@@ -1,9 +1,21 @@
 
-if [ "$1" != "-d" ]; then
-	docker run -d --name docker-hugo -v ($pwd)/blog-data:/home/blog-data:z stormzen/docker-hugo
-
-else
-	docker run -it --name docker-hugo -v ($pwd)/blog-data:/home/blog-data:z stormzen/docker-hugo
+if [ "$#" -lt 1 ]; then
+  echo 'Syntax: $0 src-directory [run]'
+  exit 1
 fi
 
+src_dir="$(pwd)/blog-data/$1/src"
+
+if [ ! -d "$1" ]; then
+  echo "Directory $1 does not exist."
+  ls "$(pwd)/blog-data"
+  exit 1
+fi
+
+if [ "$2" != "run" ]; then
+   docker run -d -p1313:1313 --name docker-hugo -v "$src_dir":/src:z stormzen/docker-hugo
+
+else
+   docker exec -it docker-hugo sh
+fi
 
