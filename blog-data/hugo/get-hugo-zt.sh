21b79897fa93bf74722f6d13e97a70135b671667
J . Norment <cognitiaclaeves@gmail.com>
2017-04-30 20:08:40 +0000

    Added zt imported to hugo

---
+++ blog-data/hugo/get-hugo-zt.sh
@@ -0,0 +1,6 @@
+
+# git clone https://github.com/cognitiaclaeves/hugo-play.git
+# git clone https://github.com/cognitiaclaeves/hugo_theme_robust.git hugo-play/src/bookshelf/themes/hugo_theme_robust
+
+git clone https://github.com/cognitiaclaeves/hugo.src.cognitiaclaeves.github.io.git
+git clone https://github.com/spf13/herring-cove.git hugo.src.cognitiaclaeves.github.io/themes/herring-cove
--- run-container.sh
+++ run-container.sh
@@ -5,6 +5,7 @@ if [ "$#" -lt 1 ]; then
 fi
 
 src_dir="$(pwd)/blog-data/$1/src"
+blg_data="$(pwd)/blog-data"
 
 if [ ! -d "$src_dir" ]; then
   echo "Directory $src_dir does not exist."
@@ -15,10 +16,10 @@ fi
 
 if [ "$2" = "run" ]; then
    docker stop docker-hugo && docker rm -v docker-hugo 2> /dev/null
-   docker run -d -p1313:1313 --name docker-hugo -v "$src_dir":/src:z stormzen/docker-hugo
+   docker run -d -p1313:1313 --name docker-hugo -v "$src_dir":/src:z -v "$blg_data":/blog_data:z stormzen/docker-hugo
 elif [ "$2" = "debug" ]; then
    docker stop docker-hugo && docker rm -v docker-hugo 2> /dev/null
-   docker run -it -p1313:1313 --name docker-hugo -v "$src_dir":/src:z stormzen/docker-hugo sh
+   docker run -it -p1313:1313 --name docker-hugo -v "$src_dir":/src:z -v "$blg_data":/blog_data:z stormzen/docker-hugo sh
 else
    docker exec -it docker-hugo sh
 fi
