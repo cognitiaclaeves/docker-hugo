5a71a3d622cfebee88bb32619910ad5ba7f90c73
J . Norment <cognitiaclaeves@gmail.com>
2017-04-30 01:59:00 +0000

    run-container changes.

--- run-container.sh
+++ run-container.sh
@@ -15,10 +15,10 @@ fi
 
 if [ "$2" = "run" ]; then
    docker stop docker-hugo && docker rm -v docker-hugo 2> /dev/null
-   docker run -d -p80:1313 --name docker-hugo -v "$src_dir":/src:z stormzen/docker-hugo
+   docker run -d -p1313:1313 --name docker-hugo -v "$src_dir":/src:z stormzen/docker-hugo
 elif [ "$2" = "debug" ]; then
    docker stop docker-hugo && docker rm -v docker-hugo 2> /dev/null
-   docker run -it -p80:1313 --name docker-hugo -v "$src_dir":/src:z stormzen/docker-hugo sh
+   docker run -it -p1313:1313 --name docker-hugo -v "$src_dir":/src:z stormzen/docker-hugo sh
 else
    docker exec -it docker-hugo sh
 fi
